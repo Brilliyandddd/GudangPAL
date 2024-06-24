462f7de72cc765058beb7747cc205e0b92c406bb
FaisAkbar <akbarfais06@gmail.com>
2024-06-24 09:11:13 +0700

    Test

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -91,7 +91,7 @@
                     <div class="d-flex">
                         <div class="text-white">
                             <h2 class="mb-0 number-font">{{ $bk }}</h2>
-                            <p class="text-white mb-0">Barang Keluar</p>
+                            <p class="text-white mb-0">Barang hguyuyv</p>
                         </div>
                         <div class="ms-auto"> <i class="fe fe-repeat text-white fs-40 me-2 mt-2"></i> </div>
                     </div>
